c4a8c3bb5626acfdc6e9b93379ef5dd83d7f448d
javawmw <awangmingwei@163.com>
2019-03-15 11:18:39 +0800

    打印调试2

--- app/src/main/java/com/hk/heichijun/fragment/NewTaskFragment.java
+++ app/src/main/java/com/hk/heichijun/fragment/NewTaskFragment.java
@@ -847,6 +847,7 @@ public class NewTaskFragment extends BaseFragement implements SwipeBackLayout.Sw
             @Override
             public void onSuccess(AppBack appBack) {
                 if (appBack.isSuccess()) {
+                    PrintOrderUtils.print(listItem.get(position));
 //                  listItem.get(position).put("cookStatus", 2);
                     myAdpter.notifyDataSetChanged();
                     page = 1;
@@ -888,7 +889,9 @@ public class NewTaskFragment extends BaseFragement implements SwipeBackLayout.Sw
             public void onSuccess(AppBack appBack) {
                 if (appBack.isSuccess()) {
                     Loge(listItem.get(position));
-                    ///PrintOrderUtils.print(listItem.get(position));//打印某一订单
+                    if (!listItem.get(position).get("orderType").equals(4)){
+                        PrintOrderUtils.print(listItem.get(position));//打印某一订单
+                    }
                     listItem.get(position).put("cookStatus", 1);
                     if (listItem.get(position).get("orderType").equals(3)){
                         listItem.get(position).put("dadaStatus","0");
--- app/src/main/java/com/hk/heichijun/fragment/OrderFragment.java
+++ app/src/main/java/com/hk/heichijun/fragment/OrderFragment.java
@@ -682,6 +682,7 @@ public class OrderFragment extends BaseActivity {
             @Override
             public void onSuccess(AppBack appBack) {
                 if (appBack.isSuccess()) {
+                    PrintOrderUtils.print(orderMap);
                     EventBus.getDefault().post(new String("catch"));
                     inist();
                 }
@@ -700,7 +701,9 @@ public class OrderFragment extends BaseActivity {
             public void onSuccess(AppBack appBack) {
                 if (appBack.isSuccess()) {
                     if (orderMap.size() > 0) {
-                        //PrintOrderUtils.print(orderMap);
+                        if (!"4".equals(orderMap.get("orderType").toString())) {
+                            PrintOrderUtils.print(orderMap);
+                        }
                         EventBus.getDefault().post(new String("catch"));
                         inist();
                     }
